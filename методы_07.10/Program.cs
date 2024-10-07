ba209f7a228868935bf1fdd19e0f3e485bf06d31
daidery <popovad848@gmail.com>
2024-10-07 13:21:25 +0500

    Ура победа

---
+++ методы_07.10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace методы_07._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение X");
+            var x = double.Parse(Console.ReadLine());
+
+            var y = MyFunction(x);
+            Console.WriteLine("f(x) = " + y);
+
+            Console.ReadKey();
+        }
+
+        static double MyFunction(double x)
+        {
+            //throw new NotImplementedException();
+            return Math.Sin((3.2 + Math.Sqrt(x + 1)) / Math.Abs(5 * x));
+        }
+    }
+}
