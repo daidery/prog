825853e2e114fe9bdf2aaaabdec4f4f35a8da10d
daidery <popovad848@gmail.com>
2025-02-12 15:52:56 +0500

    бебебе

---
+++ евклид_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Евклид;
+
+namespace евклид_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Point() { X = 1, Y = 3 }; 
+
+            var q = new Point();
+            q.X = -1;
+            q.Y = 3;
+
+            PrintPoint(p);
+            PrintPoint(q);
+
+            Console.WriteLine(new Point(5,6);
+            
+           Console.ReadKey();
+        }
+        static void PrintPoint (Point point)
+        {
+            Console.WriteLine($"Точка {point.X}; {point.Y}");
+        }
+    }
+}
---
+++ евклид_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("евклид_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("евклид_2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("6cad08ff-6cec-4374-95ea-2d862ebdae23")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
